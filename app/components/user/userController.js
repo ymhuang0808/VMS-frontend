521b66f019a3a453fb887e3820cad86423aa85c8
YMHuang <ym.huang0808@gmail.com>
2015-10-14 22:31:58 +0800

    Orz :+1

--- app/components/user/userController.js
+++ app/components/user/userController.js
@@ -7,7 +7,7 @@ app.controller('MainController', ['$scope', function($scope) {
     $scope.reContent='To complete the register process, an email confirmation was sent to your email address, '+$scope.user+'. You can confirm your email address by clicking on the URL that was provided in the confirmation email sent you.';
     $scope.reContent1='If you have not received an email confirmation message be sent to your email address, please click the resent button to request a new one.';
 
-    $scope.create = function(volunteer) {
+    $scope.registerSubmit = function(volunteer) {
     	console.log("click create");
     	console.log(volunteer.first_name);
     	console.log(volunteer.email);
